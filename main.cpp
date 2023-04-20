a7860dbe25b62d4ce4aaae810a0d99ab149501e4
Alvineornela <127629007+Alvineornela@users.noreply.github.com>
2023-04-20 16:20:40 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,6 +1,25 @@
 #include <iostream>
 using namespace std;
-
 int main() {
+char lista_nomi[3][5] = {"", "", ""};
+ char nome[5] = {""};
+ cin >> lista_nomi[0];
+ cin >> lista_nomi[1];
+ cin >> lista_nomi[2];
+ cout << endl;
+ cin >> nome;
+ if((nome[0]==lista_nomi[0][0])&&(nome[1]==lista_nomi[0][1])&&(nome[2]==lista_nomi[0][2])&&(nome[3]==lista_nomi[0][3])) {
+    cout<<"o"<<endl;
+    }
+ else if((nome[0]==lista_nomi[1][0])&&(nome[1]==lista_nomi[1][1])&&(nome[2]==lista_nomi[1][2])&&(nome[3]==lista_nomi[1][3])){
+     cout<<"1"<<endl;
+ }
+ else if((nome[0]==lista_nomi[2][0])&&(nome[1]==lista_nomi[2][1])&&(nome[2]==lista_nomi[2][2])&&(nome[3]==lista_nomi[2][3])){
+     cout<<"2"<<endl;
+ }
+else{
+    cout<<""<<endl;
+}
+
    return 0;
 }
